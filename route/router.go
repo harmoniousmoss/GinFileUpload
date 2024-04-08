c6d165d903dbc3ccd8e51bdf1aed6ae7ac6f7179
Harmoniousmoss <syaefulbahri@protonmail.com>
2024-04-08 08:49:12 +0700

    Add / route

--- route/router.go
+++ route/router.go
@@ -9,6 +9,12 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
+	// Add a GET route for the root path
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Welcome to Gin-GCS Uploader")
+	})
+
+	// POST route for file upload
 	r.POST("/upload", handler.UploadFile)
 
 	return r
